6869a12fed56e103e5634ea5d82c05a780809cdd
prasadbabu <prasadbabu@metricstream.com>
2026-04-26 22:56:08 +0530

    feat: Add JRE version metadata to JAR manifest in root gradle config
    - Added manifest configuration to root build.gradle.kts to apply to all subprojects
    - Includes Build-Jdk, Build-Jdk-Spec, and Build-Timestamp attributes
    - Developers can now easily identify compatible JVM versions from JAR metadata
    - Removed duplicate configurations from individual module build files

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,4 +8,21 @@ subprojects {
     repositories {
         mavenCentral()
     }
+
+    tasks.withType<Jar> {
+        manifest {
+            attributes(
+                "Implementation-Title" to project.name,
+                "Implementation-Version" to project.version,
+                "Specification-Title" to project.name,
+                "Specification-Version" to project.version,
+                "Bundle-Name" to project.name,
+                "Bundle-Version" to project.version,
+                "Created-By" to "${System.getProperty("java.version")} (${System.getProperty("java.vendor")})",
+                "Build-Jdk" to System.getProperty("java.version"),
+                "Build-Jdk-Spec" to System.getProperty("java.specification.version"),
+                "Build-Timestamp" to java.time.Instant.now().toString()
+            )
+        }
+    }
 }
